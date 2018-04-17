1cd73923d59b391b31cc6c85ba7364f944001890
lsl <408930131@qq.com>
2018-04-17 18:33:56 +0800

    Initial commit

---
+++ ams-plugin/src/main/groovy/com/ex/plugin/PluginImpl.groovy
@@ -0,0 +1,80 @@
+package com.ex.plugin
+
+import com.android.build.api.transform.Context
+import com.android.build.api.transform.DirectoryInput
+import com.android.build.api.transform.Format
+import com.android.build.api.transform.JarInput
+import com.android.build.api.transform.QualifiedContent
+import com.android.build.api.transform.Transform
+import com.android.build.api.transform.TransformException
+import com.android.build.api.transform.TransformInput
+import com.android.build.api.transform.TransformOutputProvider
+import com.android.build.gradle.AppExtension
+import com.android.build.gradle.internal.pipeline.TransformManager
+import org.apache.commons.codec.digest.DigestUtils
+import org.apache.commons.io.FileUtils
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+public class PluginImpl extends Transform implements Plugin<Project> {
+
+
+    @Override
+    String getName() {
+        return "PluginImpl"
+    }
+
+    @Override
+    Set<QualifiedContent.ContentType> getInputTypes() {
+        return TransformManager.CONTENT_CLASS
+    }
+
+    @Override
+    Set<? super QualifiedContent.Scope> getScopes() {
+        return TransformManager.SCOPE_FULL_PROJECT
+    }
+
+    @Override
+    boolean isIncremental() {
+        return false
+    }
+
+    @Override
+    void apply(Project project) {
+        def android = project.extensions.getByType(AppExtension)
+        android.registerTransform(this)
+    }
+
+    @Override
+    void transform(Context context, Collection<TransformInput> inputs, Collection<TransformInput> referencedInputs, TransformOutputProvider outputProvider, boolean isIncremental) throws IOException, TransformException, InterruptedException {
+        println '//===============Plugin visit start===============//'
+
+        if (outputProvider != null)
+            outputProvider.deleteAll()
+
+        inputs.each { TransformInput input ->  //对目录下的class文件插桩
+            input.directoryInputs.each { DirectoryInput directoryInput ->
+
+                //代码插桩
+                def dest = outputProvider.getContentLocation(directoryInput.name,
+                        directoryInput.contentTypes, directoryInput.scopes, Format.DIRECTORY)
+                FileUtils.copyDirectory(directoryInput.file, dest)
+            }
+
+            input.jarInputs.each { JarInput jarInput ->  //对jar文件代码插桩
+                def jarName = jarInput.name
+                def md5Name = DigestUtils.md5Hex(jarInput.file.getAbsolutePath())
+                if (jarName.endsWith(".jar")) {
+                    jarName = jarName.substring(0, jarName.length() - 4)
+                }
+                //代码插桩
+                def dest = outputProvider.getContentLocation(jarName + md5Name,
+                        jarInput.contentTypes, jarInput.scopes, Format.JAR)
+                FileUtils.copyFile(jarInput.file, dest)  //关键步骤，如果没有，出现class找不到问题
+            }
+
+        }
+        println '//===============Plugin visit end===============//'
+
+    }
+}
